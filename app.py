b378ae903f317b572f855c5820be26cc5cd9bc3a
Maria Grassa <124839784+mgrassa14@users.noreply.github.com>
2026-04-21 15:25:38 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # render_template looks in the /templates folder by default
+    return render_template('location.html')
+
+@app.route('/results')
+def result():
+    # render_template looks in the /templates folder by default
+    return render_template('results.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
