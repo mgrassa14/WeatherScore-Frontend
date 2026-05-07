19a30baa8a18c7358da138a26fe295b544753b3e
Maria Grassa <mgrassa00@gmail.com>
2026-05-06 23:50:15 -0700

    fixing getting results from db

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ COPY . .
 ENV PORT=8080
 EXPOSE 8080
 
-CMD ["gunicorn", "-b", "0.0.0.0:8080", "app:app"]
+CMD ["python", "app.py"]
--- static/js/results.js
+++ static/js/results.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const results = JSON.parse(sessionStorage.getItem("results") || "[]");
+// document.addEventListener("DOMContentLoaded", () => {
+//     const results = JSON.parse(sessionStorage.getItem("results") || "[]");
 
-    results.forEach((r, index) => {
-        const rank = index + 1;
-        document.getElementById(`name${rank}`).textContent = r.provider;
-        document.getElementById(`accuracy${rank}`).textContent = `${r.accuracy.toFixed(2)}%`;
-    });
-});
+//     results.forEach((r, index) => {
+//         const rank = index + 1;
+//         document.getElementById(`name${rank}`).textContent = r.provider;
+//         document.getElementById(`accuracy${rank}`).textContent = `${r.accuracy.toFixed(2)}%`;
+//     });
+// });
