10dce36f28f2b188e089be1eb38547d52a920cf3
Maria Grassa <mgrassa00@gmail.com>
2026-05-07 00:10:15 -0700

    location and results page work for 1 day accuracy

--- static/js/location.js
+++ static/js/location.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+    // get form
     const form = document.querySelector("form");
-
+    // form event listener
     form.addEventListener("submit", async (e) => {
         e.preventDefault();
-
+        // payload to submit
         const payload = {
             city: document.getElementById("validationServer03").value,
             state: document.getElementById("validationServer04").value,
@@ -13,16 +13,16 @@ document.addEventListener("DOMContentLoaded", () => {
         };
 
         console.log("Submitting payload:", payload);
-
+        // send payload to cloud funciton
         const response = await fetch("https://calculate-accuracy-700897000697.us-central1.run.app", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(payload)
         });
-
+        // get results into data
         const data = await response.json();
         console.log("Cloud Function response:", data);
-
+        // add results into sessionStorag for results.js/html to get
         if (data.results) {
             sessionStorage.setItem("results", JSON.stringify(data.results));
             window.location.href = "/results";
--- static/js/results.js
+++ static/js/results.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // get results from sessionStorage
     const results = JSON.parse(sessionStorage.getItem("results") || "[]");
-
+    // add results into the table in html
     results.forEach((r, index) => {
         const rank = index + 1;
         document.getElementById(`name${rank}`).textContent = r.provider;
